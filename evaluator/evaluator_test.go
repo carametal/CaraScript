5470406a3746dc3f02ebebce43278e00a6acb54b
carametal <mkatsdevelop@gmail.com>
2025-04-13 15:31:14 +0900

    evaluatorのテストをテーブル駆動テストに変更

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -1,18 +1,31 @@
 package evaluator
 
 import (
+	"carametal/CaraScript/lexer"
 	"carametal/CaraScript/parser"
-	"strconv"
 	"testing"
 )
 
 func TestEval(t *testing.T) {
-	v, _ := strconv.ParseInt("1", 10, 64)
-	p := parser.IntegerLiteral{
-		Value: v,
+	tests := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "正の整数",
+			input: "123",
+			want:  "123",
+		},
 	}
-	got := Eval(&p)
-	if got.String() != "1" {
-		t.Fatalf("Eval() want %s, got %s", "1", got.String())
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := lexer.New(tt.input)
+			p := parser.New(l)
+			got := Eval(p.ParseProgram())
+			if got.String() != tt.want {
+				t.Fatalf("Eval() want %s, got %s", "1", got.String())
+			}
+		})
 	}
 }
